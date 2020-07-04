c4c434276e907e2945b32a9b0c5e7a621d02fb89
yusrawazir554@gmail.com <40852016+hasniansaleem@users.noreply.github.com>
2020-07-04 18:08:12 +0500

    React Expense-Tracker-App

---
+++ precache-manifest.20e56902a85e36395876df4ca44c7ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b37c2d70a8d73e1caccde653f8870d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e2672b5f0a4a8dfb34d",
+    "url": "/static/css/main.76df8d97.chunk.css"
+  },
+  {
+    "revision": "1c7cceec31b17bfa9e67",
+    "url": "/static/js/2.ce5cf646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce5cf646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e2672b5f0a4a8dfb34d",
+    "url": "/static/js/main.4db68e52.chunk.js"
+  },
+  {
+    "revision": "31744f32244a9ea51ce0",
+    "url": "/static/js/runtime-main.c10e0518.js"
+  }
+]);
